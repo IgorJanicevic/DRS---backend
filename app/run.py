b7e11803877ddc59b2f7f1ee13d329c33847c84e
Igor Janicevic <milkomilkic@yahoo.com>
2025-07-01 15:05:18 +0200

    Enable multiproccessing.

--- app/run.py
+++ app/run.py
@@ -4,5 +4,4 @@ from extensions import socketio
 app = create_app()
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, port=5000, host='0.0.0.0', allow_unsafe_werkzeug=True)
-
+    socketio.run(app, host='0.0.0.0', port=5000)
